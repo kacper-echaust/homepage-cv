269fb0a76240f02db642a0ba23bec8b270094ebd
kacper-echaust <k9echaust@gmail.com>
2024-12-31 13:54:02 +0100

    create projects component

---
+++ src/Components/Projects/Projects.tsx
@@ -0,0 +1,27 @@
+import { Section } from '../Section/Section'
+import css from './Projects.module.scss'
+import { projects } from '../../data/projects'
+
+const Projects = () => {
+	return (
+		<Section sectionName='projects'>
+			<ul className={css.list}>
+				{projects.map((project, index) => {
+					return (
+						<div className={css.projectContainer}>
+							<p className={css.projectName}>{project.name}</p>
+							<a href={project.link} key={index}>
+								<img src={project.imgSrc} alt={project.name} />
+								<div className={css.description}>
+									<p>{project.description}</p>
+								</div>
+							</a>
+						</div>
+					)
+				})}
+			</ul>
+		</Section>
+	)
+}
+
+export { Projects }
---
+++ src/data/projects.ts
@@ -0,0 +1,19 @@
+import fishkappImage from '../../public/fishkapp.png'
+import barberShopImage from '../../public/barber-shop.png'
+
+export const projects = [
+	{
+		name: 'fishkapp',
+		description:
+			'Aplikacja do dodawania kart pozwala tworzyć i zarządzać kartami z treściami, takimi jak tekst, obrazy czy linki. Prosty interfejs sprawia, że świetnie nadaje się do notatek.',
+		imgSrc: fishkappImage,
+		link: 'https://kacper-echaust.github.io/fischkapp/',
+	},
+	{
+		name: 'barber shop',
+		description:
+			'Stworzona przeze mnie strona barber shopu to nowoczesny projekt z przejrzystym układem, intuicyjną nawigacją i klimatycznym designem, który odzwierciedla charakter miejsca i ułatwia rezerwację wizyt.',
+		imgSrc: barberShopImage,
+		link: 'https:// barbershopvicecity.pl',
+	},
+]
